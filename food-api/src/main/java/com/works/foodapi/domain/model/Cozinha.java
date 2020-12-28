4a942d3348a0947985d56cdc70a9e504dc6bfb6d
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-12-28 19:13:29 -0300

    add autoincremennt

--- food-api/src/main/java/com/works/foodapi/domain/model/Cozinha.java
+++ food-api/src/main/java/com/works/foodapi/domain/model/Cozinha.java
@@ -1,19 +1,15 @@
 package com.works.foodapi.domain.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "tab_cozinhas")
 public class Cozinha {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nom_cozinha")
     private String nome;
 
     public Long getId() {
