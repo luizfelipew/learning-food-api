523d0044f81d364beccb25b91bf877c48598f345
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-01-06 12:04:56 -0300

    buscando um objeto pelo id no banco de dados

---
+++ food-api/src/main/java/com/works/foodapi/jpa/BuscaCozinhaMain.java
@@ -0,0 +1,23 @@
+package com.works.foodapi.jpa;
+
+import com.works.foodapi.FoodApiApplication;
+import com.works.foodapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class BuscaCozinhaMain {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(FoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha = cadastroCozinha.buscar(1L);
+
+        System.out.println(cozinha.getNome());
+    }
+}
