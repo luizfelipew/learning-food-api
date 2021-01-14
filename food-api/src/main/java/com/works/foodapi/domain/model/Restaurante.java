8076167ae8cc200e3ddac6041625ad6561711eb2
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-01-14 17:07:20 -0300

    add propriedade nullable de @Column e @JoinColumn

--- food-api/src/main/java/com/works/foodapi/domain/model/Restaurante.java
+++ food-api/src/main/java/com/works/foodapi/domain/model/Restaurante.java
@@ -15,13 +15,14 @@ public class Restaurante {
     @Id
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
 
-    @Column(name = "taxa_frete")
+    @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
     @ManyToOne
-    @JoinColumn(name = "cozinha_id")
+    @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 
 }
