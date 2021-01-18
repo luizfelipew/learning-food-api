494ec370cdf7053deb36acaa2f1e536271d3196a
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-01-18 12:12:22 -0300

    add entity repository de Forma de pagamento e permissao

---
+++ food-api/src/main/java/com/works/foodapi/domain/repository/PermissaoRepository.java
@@ -0,0 +1,13 @@
+package com.works.foodapi.domain.repository;
+
+import com.works.foodapi.domain.model.Permissao;
+
+import java.util.List;
+
+public interface PermissaoRepository {
+
+    List<Permissao> listar();
+    Permissao buscar(Long id);
+    Permissao salvar(Permissao permissao);
+    void remover(Permissao permissao);
+}
---
+++ food-api/src/main/java/com/works/foodapi/infrastructure/repository/PermissaoRepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.works.foodapi.infrastructure.repository;
+
+import com.works.foodapi.domain.model.FormaPagamento;
+import com.works.foodapi.domain.model.Permissao;
+import com.works.foodapi.domain.repository.FormaPagamentoRepository;
+import com.works.foodapi.domain.repository.PermissaoRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Component
+public class PermissaoRepositoryImpl implements PermissaoRepository {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Permissao> listar() {
+        return manager.createQuery("from Permissao", Permissao.class)
+                .getResultList();
+    }
+
+    @Override
+    public Permissao buscar(Long id) {
+        return manager.find(Permissao.class, id);
+    }
+
+    @Transactional
+    @Override
+    public Permissao salvar(Permissao permissao) {
+        return manager.merge(permissao);
+    }
+
+    @Transactional
+    @Override
+    public void remover(Permissao permissao) {
+        permissao = buscar(permissao.getId());
+        manager.remove(permissao);
+    }
+}
