c517f52287589bb72a486997083f0106a901da4a
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-01-21 22:22:44 -0300

    desafio add um Collection Resource de estados

--- food-api/src/main/resources/import.sql
+++ food-api/src/main/resources/import.sql
@@ -6,3 +6,8 @@ insert into restaurante (id, nome, taxa_frete, cozinha_id) values (1, 'Thai Gour
 insert into restaurante (id, nome, taxa_frete, cozinha_id) values (2, 'Thai Delivery', 9.5, 1);
 insert into restaurante (id, nome, taxa_frete, cozinha_id) values (3, 'Tuk Tuk Comida Indiana', 5.5, 2);
 insert into restaurante (id, nome, taxa_frete, cozinha_id) values (4, 'Comida Mineira', 6.5, 3);
+
+
+
+insert into Estado (id, nome) values (1, 'São Paulo');
+insert into Estado (id, nome) values (2, 'Minas Gerais');
