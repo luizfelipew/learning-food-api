e50f0ad07cdf77ded6ac3bfbbeceff6a4e7fe34e
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-01-29 13:26:03 -0300

    corrigindo o Status HTTP para resource inexistente

--- food-api/src/main/java/com/works/foodapi/api/controller/CozinhaController.java
+++ food-api/src/main/java/com/works/foodapi/api/controller/CozinhaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping(value = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -36,14 +37,9 @@ public class CozinhaController {
     public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
         Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
 
-//        return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-        return ResponseEntity.ok(cozinha);
-
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
-//
-//        return ResponseEntity.status(HttpStatus.FOUND)
-//                .headers(headers)
-//                .build();
+        if (Objects.nonNull(cozinha)){
+            return ResponseEntity.ok(cozinha);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
