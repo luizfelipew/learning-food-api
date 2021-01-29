4c989e6a87631c84d3bf2bf6a4f3dde5e2f272c5
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-01-29 13:16:30 -0300

    manipulando a resposta HTTP com ResponseEntity

--- food-api/src/main/java/com/works/foodapi/api/controller/CozinhaController.java
+++ food-api/src/main/java/com/works/foodapi/api/controller/CozinhaController.java
@@ -4,7 +4,10 @@ import com.works.foodapi.api.model.CozinhasXmlWrapper;
 import com.works.foodapi.domain.model.Cozinha;
 import com.works.foodapi.domain.repository.CozinhaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +33,17 @@ public class CozinhaController {
     }
 
     @GetMapping("/{cozinhaId}")
-    public Cozinha buscar(@PathVariable Long cozinhaId) {
-        return cozinhaRepository.buscar(cozinhaId);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+//        return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+        return ResponseEntity.ok(cozinha);
+
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+//
+//        return ResponseEntity.status(HttpStatus.FOUND)
+//                .headers(headers)
+//                .build();
     }
 }
