48e1bc8fea3f3c27f8352ef2725c7c798a3aad4f
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-02-01 09:53:08 -0300

    modelando e implementando a inclusão de recursos com POST

--- food-api/src/main/java/com/works/foodapi/api/controller/CozinhaController.java
+++ food-api/src/main/java/com/works/foodapi/api/controller/CozinhaController.java
@@ -4,14 +4,10 @@ import com.works.foodapi.api.model.CozinhasXmlWrapper;
 import com.works.foodapi.domain.model.Cozinha;
 import com.works.foodapi.domain.repository.CozinhaRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
@@ -41,5 +37,11 @@ public class CozinhaController {
             return ResponseEntity.ok(cozinha);
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+        return cozinhaRepository.salvar(cozinha);
     }
 }
