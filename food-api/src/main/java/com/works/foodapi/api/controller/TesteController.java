71c01aa78d804b230bdc9c3d3b8d9313ebaafdec
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-03-03 10:08:24 -0300

    criando consultas com query methods

--- food-api/src/main/java/com/works/foodapi/api/controller/TesteController.java
+++ food-api/src/main/java/com/works/foodapi/api/controller/TesteController.java
@@ -1,9 +1,15 @@
 package com.works.foodapi.api.controller;
 
+import com.works.foodapi.domain.model.Cozinha;
 import com.works.foodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/teste")
@@ -12,5 +18,15 @@ public class TesteController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
+        return cozinhaRepository.findTodasByNome(nome);
+    }
+
+    @GetMapping("/cozinhas/unica-por-nome")
+    public Optional<Cozinha> cozinhaPorNome(@RequestParam("nome") String nome) {
+        return cozinhaRepository.findByNome(nome);
+    }
+    
 
 }
