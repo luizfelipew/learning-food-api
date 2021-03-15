d02769fd742003c5fb09d048c292795e31356e05
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-03-15 19:04:22 -0300

    analisando o impacto do relacionamento muitos-para-muitos na REST API

--- food-api/src/main/java/com/works/foodapi/api/controller/RestauranteController.java
+++ food-api/src/main/java/com/works/foodapi/api/controller/RestauranteController.java
@@ -59,7 +59,8 @@ public class RestauranteController {
         Optional<Restaurante> restauranteAtual = restauranteRepository.findById(restauranteId);
 
         if (restauranteAtual.isPresent()) {
-            BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id");
+            BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id",
+                    "formasPagamento");
             try {
                 Restaurante restauranteSalva = cadastroRestaurante.salvar(restauranteAtual.get());
                 return ResponseEntity.ok(restauranteSalva);
