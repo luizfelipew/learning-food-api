9054ce2746282782cfe194b1e1bd8fe2621978ad
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-03-30 16:52:00 -0300

    adicionando dados de testes com callback do Flyway

--- food-api/src/main/java/com/works/foodapi/domain/repository/RestauranteRepository.java
+++ food-api/src/main/java/com/works/foodapi/domain/repository/RestauranteRepository.java
@@ -15,7 +15,8 @@ public interface RestauranteRepository extends CustomJpaRepository<Restaurante, 
         RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
 
 //    @Query("from Restaurante r join r.cozinha join fetch r.formasPagamento")
-    @Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+//    @Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+    @Query("from Restaurante r join fetch r.cozinha")
     List<Restaurante> findAll();
 
     List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
