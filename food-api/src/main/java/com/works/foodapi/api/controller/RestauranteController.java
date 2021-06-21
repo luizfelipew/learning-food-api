926a45ee8138967dcfb7b36ccc451940788e5ff5
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-06-21 20:16:22 -0300

    adicionando constraints e validando no controller com @Valid

--- food-api/src/main/java/com/works/foodapi/api/controller/RestauranteController.java
+++ food-api/src/main/java/com/works/foodapi/api/controller/RestauranteController.java
@@ -17,6 +17,7 @@ import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
@@ -41,7 +42,7 @@ public class RestauranteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Restaurante adicionar(@RequestBody Restaurante restaurante) {
+    public Restaurante adicionar(@RequestBody @Valid Restaurante restaurante) {
         try {
             return cadastroRestaurante.salvar(restaurante);
         } catch (EntidadeNaoEncontradaException ex) {
