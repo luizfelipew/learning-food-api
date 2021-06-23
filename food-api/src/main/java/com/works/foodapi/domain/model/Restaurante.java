e97b952d45e9dc6db4b8415a58e0b0f749f28fc0
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-06-22 23:33:11 -0300

    conhecendo e adicionando mais constraints de validação no modelo

--- food-api/src/main/java/com/works/foodapi/domain/model/Restaurante.java
+++ food-api/src/main/java/com/works/foodapi/domain/model/Restaurante.java
@@ -8,7 +8,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -24,10 +24,14 @@ public class Restaurante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
+//    @NotNull
+//    @NotEmpty
+    @NotBlank
     @Column(nullable = false)
     private String nome;
 
+//    @DecimalMin("0")
+    @PositiveOrZero
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
