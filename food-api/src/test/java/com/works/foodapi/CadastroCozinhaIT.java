dd5917bc7c31f01ec25b8652eefe3fb62460a9f4
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-08-19 11:48:44 -0300

    validando o corpo da resposta HTTP

--- food-api/src/test/java/com/works/foodapi/CadastroCozinhaIT.java
+++ food-api/src/test/java/com/works/foodapi/CadastroCozinhaIT.java
@@ -9,7 +9,9 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -31,5 +33,21 @@ class CadastroCozinhaIT {
                 .get()
                 .then()
                 .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+        // para mostrar o log se falhar
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .body("", hasSize(4))
+                .body("nome", hasItems("Indiana", "Tailandesa"));
     }
 }
