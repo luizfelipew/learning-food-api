73fe916ada5086806f723f086d3e4fd1330dd3b7
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-09-16 20:47:18 -0300

    customizando o mapeamento de propriedades com ModelMapper

--- food-api/src/main/java/com/works/foodapi/core/modelmapper/ModelMapperConfig.java
+++ food-api/src/main/java/com/works/foodapi/core/modelmapper/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package com.works.foodapi.core.modelmapper;
 
+import com.works.foodapi.api.model.RestauranteModel;
+import com.works.foodapi.domain.model.Restaurante;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,11 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+        var modelMapper = new ModelMapper();
+
+        modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+                .addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+
+        return modelMapper;
     }
 }
