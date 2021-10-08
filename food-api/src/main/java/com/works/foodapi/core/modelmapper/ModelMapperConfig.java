6995ab019d429007b32e7c5464def8ee2b574b22
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-10-08 17:49:16 -0300

    adicionando endereço no modelo da representação do recurso de restaurante

--- food-api/src/main/java/com/works/foodapi/core/modelmapper/ModelMapperConfig.java
+++ food-api/src/main/java/com/works/foodapi/core/modelmapper/ModelMapperConfig.java
@@ -1,7 +1,8 @@
 package com.works.foodapi.core.modelmapper;
 
-import com.works.foodapi.api.model.RestauranteModel;
-import com.works.foodapi.domain.model.Restaurante;
+import com.works.foodapi.api.model.EnderecoModel;
+import com.works.foodapi.domain.model.Endereco;
+import lombok.val;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +14,15 @@ public class ModelMapperConfig {
     public ModelMapper modelMapper(){
         var modelMapper = new ModelMapper();
 
-        modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
-                .addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+//        modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+//                .addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+
+        val enderecoToEnderecoModel = modelMapper.createTypeMap(
+                Endereco.class, EnderecoModel.class);
+
+        enderecoToEnderecoModel.<String>addMapping(
+                enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+                (enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
 
         return modelMapper;
     }
