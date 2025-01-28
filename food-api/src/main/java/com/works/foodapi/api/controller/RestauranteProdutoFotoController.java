373e19ff74242d9ced669c2a445276fa51730452
luizfelipew <luizfelipewendt@gmail.com>
2025-01-28 16:42:36 -0300

    Validando o tamanho máximo do arquivo via notação

--- food-api/src/main/java/com/works/foodapi/api/controller/RestauranteProdutoFotoController.java
+++ food-api/src/main/java/com/works/foodapi/api/controller/RestauranteProdutoFotoController.java
@@ -5,9 +5,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.nio.file.Path;
 import java.util.UUID;
 
@@ -20,7 +23,7 @@ public class RestauranteProdutoFotoController {
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public void atualizarFoto(@PathVariable Long restauranteId,
                               @PathVariable Long produtoId,
-                              FotoProdutoInput fotoProdutoInput){
+                              @Valid FotoProdutoInput fotoProdutoInput) {
         val nomeArquivo = UUID.randomUUID()
                 + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
 
