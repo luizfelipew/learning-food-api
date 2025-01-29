6f89bff1d2db0fb4b88503ff5321cf3a25e4dff7
luizfelipew <luizfelipewendt@gmail.com>
2025-01-29 12:03:42 -0300

    Desafio: Validando o content type do arquivo

---
+++ food-api/src/main/java/com/works/foodapi/core/validation/FileContentTypeValidator.java
@@ -0,0 +1,25 @@
+package com.works.foodapi.core.validation;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class FileContentTypeValidator implements ConstraintValidator<FileContentType, MultipartFile> {
+
+    private List<String> allowedContentTypes;
+
+    @Override
+    public void initialize(FileContentType constraintAnnotation) {
+        this.allowedContentTypes = Arrays.asList(constraintAnnotation.allowed());
+    }
+
+    @Override
+    public boolean isValid(MultipartFile multipartFile, ConstraintValidatorContext context) {
+        return Objects.isNull(multipartFile)
+                || this.allowedContentTypes.contains(multipartFile.getContentType());
+    }
+}
