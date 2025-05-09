cf9bb0b15a6ba6cfbea0428fe35e068403a633f1
luizfelipew <luizfelipewendt@gmail.com>
2025-05-08 22:10:20 -0300

    Definindo bean do client da Amazon S3 e configurando credenciais

--- food-api/src/main/java/com/works/foodapi/infrastructure/service/storage/S3FotoStorageService.java
+++ food-api/src/main/java/com/works/foodapi/infrastructure/service/storage/S3FotoStorageService.java
@@ -1,13 +1,17 @@
 package com.works.foodapi.infrastructure.service.storage;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.works.foodapi.domain.service.FotoStorageService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
 
 @Service
+@RequiredArgsConstructor
 public class S3FotoStorageService implements FotoStorageService {
 
+    private final AmazonS3 amazonS3;
 
     @Override
     public InputStream recuperar(String nomeArquivo) {
